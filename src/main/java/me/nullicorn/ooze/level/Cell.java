fc3f5abf3627094c55995f945b2c73cea0c0a8b4
Nullicorn <bennullicorn@gmail.com>
2021-08-15 19:53:03 -0400

    Add copy method to Cell

--- src/main/java/me/nullicorn/ooze/level/Cell.java
+++ src/main/java/me/nullicorn/ooze/level/Cell.java
@@ -1,6 +1,9 @@
 package me.nullicorn.ooze.level;
 
 import com.github.ooze.protos.CellData;
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
 
 /**
  * A 16x16x16 cube of blocks.
@@ -51,5 +54,97 @@ public class Cell {
         .setPaletteName(palette.getName())
         .setBlocks(blocks.toProto())
         .build();
+  }
+
+  /**
+   * Creates an identical cell to the current one, but using a new palette that <em>only</em>
+   * contains the states used by the cell.
+   * <p><br>
+   * This can be useful if multiple cells share a palette, but only one cell is needed at a time
+   * (such as in Minecraft's level format, where every section has its own palette).
+   * <p><br>
+   * In some cases this may also decrease the size of the block array because fewer states will need
+   * to be referenced, meaning a lower {@link Palette#magnitude() magnitude}.
+   *
+   * @return an identical cell with its own palette.
+   */
+  public Cell isolatedCopy() {
+    int arrayLength = blocks.size();
+    int paletteSize = palette.size();
+
+    // Determine which (and how many) of the palette's
+    // states are being used in the block array.
+    //
+    // Each index in the BitSet corresponds to the state
+    // at the same index in the palette.
+    BitSet isStateUsed = new BitSet(paletteSize);
+    for (int i = 0; i < arrayLength; i++) {
+      int state = blocks.get(i);
+
+      // Mark the index as "used" (as long as it's in
+      // bounds).
+      if (state >= 0 && state < paletteSize) {
+        isStateUsed.set(state, true);
+      }
+    }
+    int numberOfUsedStates = isStateUsed.cardinality();
+
+    // A list all the states indicated by the isStateUsed
+    // BitSet, in the same order.
+    List<BlockState> isolatedPalette = new ArrayList<>(numberOfUsedStates);
+
+    // Represents the same "blocks" as the original array,
+    // but using the isolated palette.
+    PackedUIntArray isolatedArray;
+
+    // If all states are used, then we can short-circuit
+    // by just copying the existing palette & blocks.
+    if (numberOfUsedStates >= paletteSize) {
+      // If all states are used, then the existing
+      // palette & array can just be copied.
+      palette.forEach(isolatedPalette::add);
+      isolatedArray = blocks;
+
+    } else {
+      // A map for keeping track of each state's index
+      // in the original palette and the isolated one.
+      ArrayUIntMap newIndices = new ArrayUIntMap(numberOfUsedStates);
+
+      // Only copy the "used" states into the isolated
+      // palette.
+      for (int i = 0; i < paletteSize; i++) {
+        if (isStateUsed.get(i)) {
+          BlockState state = palette.get(i);
+
+          isolatedPalette.add(state);
+          newIndices.set(i, isolatedPalette.size() - 1);
+        }
+      }
+
+      // Recreate the "blocks" array using the isolated
+      // palette
+      int[] tempIsolatedArray = new int[arrayLength];
+      for (int i = 0; i < arrayLength; i++) {
+        int state = blocks.get(i);
+        int newState = newIndices.get(state);
+
+        // If for whatever reason the state wasn't
+        // remapped at all, set it to 0.
+        tempIsolatedArray[i] = (newState == -1)
+            ? 0
+            : newState;
+      }
+
+      // And pack the array.
+      isolatedArray = new PackedUIntArray(tempIsolatedArray);
+    }
+
+    String paletteName = "ooze:isolated_" + Integer.toHexString(hashCode());
+    int paletteVersion = palette.getDataVersion();
+
+    return new Cell(
+        new Palette(paletteName, paletteVersion, isolatedPalette),
+        isolatedArray
+    );
   }
 }
